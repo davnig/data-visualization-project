c50a1383dd796159c4affb5daeff26d79eec24ac
davnig <daddets97@gmail.com>
2023-01-30 12:38:51 +0100

    count win/draw/lose rates for all matches

--- second_plot.py
+++ second_plot.py
@@ -11,16 +11,20 @@ def encode_match_result(team, home_team, away_team, result):
 
 
 def generate_second_plot_data():
-    def count_all_winning_streaks():
+    def count_all_streaks():
         def init_streak_data():
-            for i in range(1, 21):
+            for i in range(0, 21):
                 streak_data['{}-streaks'.format(i)] = 0
                 streak_data['{}-streak-win'.format(i)] = 0
                 streak_data['{}-streak-draw'.format(i)] = 0
                 streak_data['{}-streak-lose'.format(i)] = 0
 
         def count_team_winning_streaks():
-            def save_last_streaks():
+            def save():
+                streak_data['0-streaks'] += 1
+                streak_data['0-streak-{}'.format(value)] += 1
+
+            def save_current_streak():
                 if '{}-streaks'.format(streak_count) not in streak_data:
                     return
                 streak_data['{}-streaks'.format(streak_count)] += 1
@@ -32,11 +36,12 @@ def generate_second_plot_data():
             results = team_df.values.flatten()
             streak_count = 0
             for idx, value in enumerate(results):
+                save()
                 if value == 'win':
                     streak_count += 1
                     continue
                 if streak_count > 0:
-                    save_last_streaks()
+                    save_current_streak()
                     streak_count = 0
 
         all_seasons = df.season.unique()
@@ -55,39 +60,40 @@ def generate_second_plot_data():
         return streak_data
 
     def remove_unwanted_streaks(max_streak=20):
-        for i in range(1, int(len(winning_streaks) / 4) + 1):
+        saved_max_streak = int(len(streaks) / 4)
+        for i in range(0, saved_max_streak):
             if i > max_streak:
-                winning_streaks.pop('{}-streaks'.format(i))
-                winning_streaks.pop('{}-streak-win'.format(i))
-                winning_streaks.pop('{}-streak-draw'.format(i))
-                winning_streaks.pop('{}-streak-lose'.format(i))
-        return winning_streaks
+                streaks.pop('{}-streaks'.format(i))
+                streaks.pop('{}-streak-win'.format(i))
+                streaks.pop('{}-streak-draw'.format(i))
+                streaks.pop('{}-streak-lose'.format(i))
+        return streaks
 
     def normalize_winning_streak():
         normalized = {}
-        num_of_streaks = int(len(winning_streaks) / 4)
-        for i in range(1, num_of_streaks + 1):
-            normalized['{}-win'.format(i)] = winning_streaks['{}-streak-win'.format(i)] / winning_streaks[
+        num_of_streaks = int(len(streaks) / 4)
+        for i in range(0, num_of_streaks):
+            normalized['{}-win'.format(i)] = streaks['{}-streak-win'.format(i)] / streaks[
                 '{}-streaks'.format(i)]
-            normalized['{}-draw'.format(i)] = winning_streaks['{}-streak-draw'.format(i)] / winning_streaks[
+            normalized['{}-draw'.format(i)] = streaks['{}-streak-draw'.format(i)] / streaks[
                 '{}-streaks'.format(i)]
-            normalized['{}-lose'.format(i)] = winning_streaks['{}-streak-lose'.format(i)] / winning_streaks[
+            normalized['{}-lose'.format(i)] = streaks['{}-streak-lose'.format(i)] / streaks[
                 '{}-streaks'.format(i)]
         return normalized
 
     def convert_streak_dict_to_df():
         streak_df = pd.DataFrame()
-        streak_df['streak'] = range(1, int(len(winning_streaks) / 4) + 1)
+        streak_df['streak'] = range(0, int(len(streaks) / 4))
         streak_df['win'] = [normalized_streak_count[x] for x in normalized_streak_count if x.endswith('win')]
         streak_df['draw'] = [normalized_streak_count[x] for x in normalized_streak_count if x.endswith('draw')]
         streak_df['lose'] = [normalized_streak_count[x] for x in normalized_streak_count if x.endswith('lose')]
-        streak_df['total'] = [winning_streaks[x] for x in winning_streaks if x.endswith('streaks')]
+        streak_df['total'] = [streaks[x] for x in streaks if x.endswith('streaks')]
         return streak_df
 
     df = pd.read_csv('dataset/data.csv', sep=',')
     df = df.filter(regex='season|round|home_team|away_team|result', axis=1)
-    winning_streaks = count_all_winning_streaks()
-    winning_streaks = remove_unwanted_streaks(max_streak=7)
+    streaks = count_all_streaks()
+    streaks = remove_unwanted_streaks(max_streak=7)
     normalized_streak_count = normalize_winning_streak()
     normalized_streak_df = convert_streak_dict_to_df()
     return normalized_streak_df
