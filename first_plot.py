9204ec082dc68a885c8fa86b4cca9b5845cdc35f
davnig <daddets97@gmail.com>
2023-02-13 17:02:17 +0100

    generate revised first plot data

--- first_plot.py
+++ first_plot.py
@@ -17,6 +17,22 @@ def generate_first_plot_data():
     return df, mean
 
 
+def generate_first_plot_revised_data():
+    df = pd.read_csv('dataset/data.csv', sep=',')
+    df = df[['home_fouls', 'away_fouls', 'result']]
+    df = pd.melt(df, id_vars='result', value_vars=['home_fouls', 'away_fouls'])
+    df = df.rename(columns={'variable': 'team', 'value': 'num_of_fouls'})
+    df['victory'] = df.apply(lambda x: x.result in x.team, axis=1)
+    mean = round(df['num_of_fouls'].mean())
+    df['aggressive'] = False
+    df.loc[df['num_of_fouls'] > mean, 'aggressive'] = True
+    df = df.drop(columns=['result', 'team', 'num_of_fouls'])
+    aggressive_games_count = df['aggressive'].value_counts()
+    aggressive_victories_count = df[df['aggressive']]['victory'].value_counts()
+    non_aggressive_victories_count = df[df['aggressive'] == False]['victory'].value_counts()
+    return aggressive_games_count, aggressive_victories_count, non_aggressive_victories_count
+
+
 def first_plot():
     df, mean = generate_first_plot_data()
     fig = go.Figure(
@@ -32,5 +48,10 @@ def first_plot():
     fig.show()
 
 
+def first_plot_revised():
+    aggressive_games_count, aggressive_wins_count, non_aggressive_wins_count = generate_first_plot_revised_data()
+    pass
+
+
 if __name__ == '__main__':
-    first_plot()
+    first_plot_revised()
